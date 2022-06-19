7a4881bbba8e1f876930c0614a97e8a1daa29dea
Calvin <calvinkioko91@gmail.com>
2022-06-19 21:05:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  
+  let newWord = word.toLowerCase();
+  let newWordRevevse = newWord.split("").reverse().join("");
+  return newWord === newWordRevevse;
 }
 
 /* 
   Add your pseudocode here
+  variable newWord is word.toLowerCase();
+  variable newWordRevevse is by changing newWord.split("") then reverse() and later join("");
+  strictly compare newWord and newWordRevevse
 */
 
 /*
-  Add written explanation of your solution here
+  Solution steps
+  1. Change word to lowercase letters
+  2. Split the lowercase letter words, reverse them and later join them
+  3. Compare the reversed word with the original word
 */
 
 // You can run `node index.js` to view these console logs
